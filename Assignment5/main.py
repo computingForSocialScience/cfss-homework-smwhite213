50c97b376563ea8735fe720e5e97111706d32681
smwhite213 <smwhite213@github.com>
2015-02-17 21:38:56 -0600

    final main.py

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 from fetchArtist import fetchArtistId, fetchArtistInfo
 from fetchAlbums import fetchAlbumIds, fetchAlbumInfo
 from csvUtils import writeArtistsTable, writeAlbumsTable
@@ -7,6 +8,25 @@ from barChart import plotBarChart
 if __name__ == '__main__':
     artist_names = sys.argv[1:]
     print "input artists are ", artist_names
-    # YOUR CODE HERE
+    
+    artistlist=[]
+    album_list=[]
+    for i in range(len(artist_names)):
+    	artistinfo=fetchArtistInfo(fetchArtistId(artist_names[i]))
+
+    	artistlist.append(artistinfo)
+    	artist_id=artistinfo['id']
+    	albums=fetchAlbumIds(artist_id)
+    	
+    	for album in albums:
+    
+    		albuminfo=fetchAlbumInfo(album)
+
+    		album_list.append(albuminfo)
+ 	
+    writeAlbumsTable(album_list)
+    writeArtistsTable(artistlist)
+
+plotBarChart()
     
 
